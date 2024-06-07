9b96d976430bc002303e779fa1ddd6a2556262f0
Shittu24 <ibrahimshittu247@gmail.com>
2024-06-07 19:44:23 -0400

    pipeline-coma

--- book-network/src/main/java/com/shittu/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/shittu/book/BookNetworkApiApplication.java
@@ -5,13 +5,14 @@ import com.shittu.book.role.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
-@SpringBootApplication
+@SpringBootApplication(exclude = { MailSenderAutoConfiguration.class })
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
